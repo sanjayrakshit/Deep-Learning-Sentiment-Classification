1bf4f59f9d4cf1e139523a2e6c7917239e67f8f2
Sanjay Rakshit <sanjayrakshit@Sanjays-MacBook-Air.local>
2018-10-03 19:17:28 +0530

    Introduced some modifications

--- Architecture.py
+++ Architecture.py
@@ -2,23 +2,26 @@ from load import Load_data
 from config import *
 
 import tensorflow as tf 
-import time
+import time, pickle, sys
 
-load = Load_data(batch_size, sequence_length)
-load.create_ids()
-load.parse_file()
+
+load = None
+if sys.argv[-1] == "0":
+	load = Load_data(batch_size, sequence_length)
+	load.create_ids()
+	load.parse_file()
+	with open("object.Load_data", "wb") as f:
+		pickle.dump(load, f)
+else:
+	with open("object.Load_data", "rb") as f:
+		load = pickle.load(f)
+
 
 vocab_size = len(load.wids) + 2
 
 tf.reset_default_graph()
-x = tf.placeholder(dtype=tf.int64, shape=[None, sequence_length], name="x")
-y = tf.placeholder(dtype=tf.float32, shape=[None, num_classes], name="y")
-
-
-def get_embedding():
-	with tf.variable_scope('get_embedding', reuse=tf.AUTO_REUSE):
-		word_embedding = tf.get_variable('word_embedding', [vocab_size, word_embedding_size])
-		return word_embedding
+x = tf.placeholder(dtype=tf.int64, shape=[None, None], name="x")
+y = tf.placeholder(dtype=tf.float32, shape=[None, None], name="y")
 
 
 def get_lstm_cell():
@@ -34,25 +37,23 @@ def create_rnn():
 
 def model():
 	# getting the embedding
-	word_embedding = get_embedding()
-	embedded_words = tf.nn.embedding_lookup(word_embedding, x)
+	with tf.name_scope("embedding"):
+		word_embedding = tf.Variable(tf.random_normal(shape=[vocab_size, word_embedding_size]))
+		embedded_words = tf.nn.embedding_lookup(word_embedding, x)
 	
 	# creating the rnn structure
 	CELL = create_rnn()
 	with tf.variable_scope('model', reuse=tf.AUTO_REUSE):
 		OUTPUT, FINAL_STATE = tf.nn.dynamic_rnn(CELL, embedded_words, dtype=tf.float32)
 
-	OUTPUT = tf.transpose(OUTPUT, [1,0,2]) # Step 1 to get the value from last time step
-	OUTPUT = tf.gather(OUTPUT, OUTPUT.get_shape()[0]-1) # Step 2 to get the value from last time step
-
 	# fully connected layers
-	dense = tf.contrib.layers.fully_connected(OUTPUT, num_outputs=dense_layer_size,\
+	dense = tf.contrib.layers.fully_connected(OUTPUT[:, -1], num_outputs=dense_layer_size,\
 	activation_fn=tf.nn.leaky_relu, weights_initializer=tf.keras.initializers.he_normal(),\
 	biases_initializer=tf.zeros_initializer())
 	
 	dense = tf.contrib.layers.dropout(dense, keep_prob=dropratio_dense)
 
-	prediction = tf.contrib.layers.fully_connected(dense, num_outputs=num_classes, activation_fn=tf.nn.softmax,
+	prediction = tf.contrib.layers.fully_connected(dense, num_outputs=num_classes, activation_fn=None,
 												weights_initializer=tf.truncated_normal_initializer(),
 												biases_initializer=tf.zeros_initializer())
 	return prediction
@@ -66,8 +67,9 @@ print("The trainable variables are ... ")
 print(tf.trainable_variables())
 
 with tf.name_scope('x_ent'):
-	cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=y_hats)\
-	+ tf.add_n([l2_reg_const*tf.nn.l2_loss(V) for V in tf.trainable_variables()]))
+	cost = tf.losses.softmax_cross_entropy(onehot_labels=y, logits=y_hats)
+	# cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=tf.stop_gradient(y), logits=y_hats)\
+	# + tf.add_n([l2_reg_const*tf.nn.l2_loss(V) for V in tf.trainable_variables()]))
 	tf.summary.scalar('x_ent', cost)
 
 with tf.variable_scope('train_step', reuse=tf.AUTO_REUSE):
@@ -126,12 +128,12 @@ for i in range(epoch):
 	test_loss.append(cc/no_of_ts_batches)
 	print('')
 
-print('Train loss:', train_loss[-1])
-print('Validation loss:', test_loss[-1])
-temp_test = list(zip(*load.test))
-print('Validation accuracy:', sess.run(
-	accuracy, {
-		x: temp_test[0], y: temp_test[1]
-	}
-))
-print('='*40)
+	print('Train loss:', train_loss[-1])
+	print('Validation loss:', test_loss[-1])
+	temp_test = list(zip(*load.test))
+	print('Validation accuracy:', sess.run(
+		accuracy, {
+			x: temp_test[0], y: temp_test[1]
+		}
+	))
+	print('='*40)
